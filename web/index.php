060e91fe8720f0f39ecbd2831630c225c6b078be
netjoin <netjoin@gmail.com>
2019-08-18 21:49:59 +0800

    可以切换 autoload 自动加载器

--- web/index.php
+++ web/index.php
@@ -1,15 +1,19 @@
 <?php
 
 $_CONFIG = require __DIR__ . '/../app/config.php';
-/*
-$Composer = require dirname(__DIR__) . '/vendor/autoload.php';
-*/
-$autoload = require dirname(__DIR__) . '/vendor/wuding/anfora/src/autoload.php';
-# include "E:/www/work/netjoin/astrology/src/autoload.php";
-# include "E:\www\work\wuding\anfora\src/autoload.php";
-$anfora = new \Anfora;
 
-// 依赖函数
-func($_CONFIG['func']['config'], $_CONFIG ['func']['load']);
+if ('anfora' == $_CONFIG['autoload']) {
+    $autoload = require dirname(__DIR__) . '/vendor/wuding/anfora/src/autoload.php';
+    # include "E:/www/work/netjoin/astrology/src/autoload.php";
+    # include "E:\www\work\wuding\anfora\src/autoload.php";
+    $anfora = new \Anfora;
+
+    // 依赖函数
+    func($_CONFIG['func']['config'], $_CONFIG ['func']['load']);
+
+} else {
+    $Composer = require dirname(__DIR__) . '/vendor/autoload.php';
+}
 
 Cancer::php();
+# print_r(get_included_files());
